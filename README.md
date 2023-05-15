567f9dda6f1ffbf67d4961fc6cda75162e44fa37
pealexan <pealexan@student.42porto.com>
2023-05-15 14:52:07 +0100

    final version

--- README.md
+++ README.md
@@ -1,15 +1,2 @@
-# Minishell
-The objective of this project is for you to create a simple shell.  
-  
-Started:  
-03/04/2023  
-  
-First submission:  
-03/05/2023  
-Score: 0%  
-  
-Failed due to a simple mistake on calling for ft_strncmp during built_in check. 
-  
-Second submission:  
-08/05/2023  
-Score: xxx%  
+# 42Cursus-03-Minishell
+The objective of this project is for you to create a simple shell.
--- srcs/expander.c
+++ srcs/expander.c
@@ -6,7 +6,7 @@
 /*   By: pealexan <pealexan@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 15:26:25 by pealexan          #+#    #+#             */
-/*   Updated: 2023/05/01 10:26:23 by pealexan         ###   ########.fr       */
+/*   Updated: 2023/05/15 08:55:41 by pealexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@ static void	total_length2(char *arg, int *i, int *len, t_minishell *mini)
 			j++;
 		temp = ft_substr(arg, *i + 1, j);
 		value = get_info_env(&mini->env, temp);
+		if (j == 0)
+			value = "$";
 		if (value)
 			*len += ft_strlen(value);
 		free(temp);
@@ -33,7 +35,7 @@ static void	total_length2(char *arg, int *i, int *len, t_minishell *mini)
 	else
 	{
 		temp = ft_itoa(g_exit_status);
-		len += ft_strlen(temp);
+		*len += ft_strlen(temp);
 		free(temp);
 		*i += 1;
 	}
@@ -86,6 +88,8 @@ static void	expand_variable(char *arg, char *result, int *i, t_minishell *mini)
 			len++;
 		temp = ft_substr(arg, *i + 1, len);
 		value = get_info_env(&mini->env, temp);
+		if (len == 0)
+			value = "$";
 		free(temp);
 		*i += len;
 		if (value)
